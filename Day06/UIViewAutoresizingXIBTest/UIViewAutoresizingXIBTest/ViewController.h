3321802b15a54d1af315308a8d79fc2d89d7d4c5
xiaotuan <583168425@qq.com>
2018-07-14 10:52:34 +0800

    添加在XIB文件中使用Autoresizing功能的示例代码

---
+++ Day06/UIViewAutoresizingXIBTest/UIViewAutoresizingXIBTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewAutoresizingXIBTest
+//
+//  Created by QinTuanye on 2018/7/13.
+//  Copyright © 2018年 QinTuanye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Day06/UIViewAutoresizingXIBTest/UIViewAutoresizingXIBTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UIViewAutoresizingXIBTest
+//
+//  Created by QinTuanye on 2018/7/13.
+//  Copyright © 2018年 QinTuanye. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
